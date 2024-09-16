99f90ecd7c35d12868480d16b25472acdada5400
mernstackproject <rammohangupta1769@gmail.com>
2024-09-16 17:44:49 +0530

    changes

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Page = () => {
   return (
     <div className=' h-20 text-center'>
-    <h1 className='text-2xl decoration-green-500  '>This about page</h1>
+    <h1 className='text-2xl decoration-green-500  '>This about page routing</h1>
     </div>
 
   )
